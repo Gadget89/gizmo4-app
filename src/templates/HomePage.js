caa34829560afde12ed8b811ed2a52bfe921f594
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-03-26 09:30:44 -0600

    Removed unnecessary component from template.

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -55,22 +55,16 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
   <main className="Home">
-    <PageHeader
-      large
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
 
     <section className="section">
       <div className="container">
+        {/* Navigation cards will go here. */}
         <Content source={body} />
       </div>
     </section>

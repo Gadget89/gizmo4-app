8bfe080ca754fa95c9f327aa747717f4de522cb2
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-05-15 22:05:49 -0500

    Updated scaffolding for cube component.

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,32 +1,66 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Image from './Image'
-import './PostCard.css'
+import Image from './CubeImage'
+import './Cubes.css'
 
-const PostCard = ({
+// To-Do:
+// [x] Establish data needed for each cube container.
+// [x] Import cube structure from template.
+// [x] Pass data into component.
+// [ ] Test that component works as designed.
+
+const LinkContainer = ({
+  slug,
   featuredImage,
   title,
-  excerpt,
-  slug,
-  categories = [],
+  subtitle,
+  id,
+  gist,
+  buttonText,
   className = '',
+  frontImage,
+  sideImage,
+  backImage,
   ...props
 }) => (
-  <Link to={slug} className={`PostCard ${className}`}>
-    {featuredImage && (
-      <div className="PostCard--Image relative">
-        <Image background src={featuredImage} alt={title} />
-      </div>
-    )}
-    <div className="PostCard--Content">
-      {title && <h3 className="PostCard--Title">{title}</h3>}
-      <div className="PostCard--Category">
-        {categories && categories.map(cat => cat.category).join(', ')}
-      </div>
-      {excerpt && <div className="PostCard--Excerpt">{excerpt}</div>}
+  <div key={id} className="cube-container">
+    <div className="photo-cube">
+      <Link to={slug}>
+        <div className="front photo-desc">
+          {title && <h3 className="secondary-header">{title}</h3>}
+          {frontImage && <Image resolutions="small" src={frontImage} alt="" />}
+        </div>
+      </Link>
+      <Link to={slug}>
+        <div className="back photo-desc">
+          {title && <h3 className="secondary-header">{title}</h3>}
+          {gist && (
+            <p
+              style={{
+                margin: '42px 0',
+                minHeight: '88px',
+                lineHeight: '1.25'
+              }}
+            >
+              {gist}
+            </p>
+          )}
+          {buttonText && <span className="call-to-action">{buttonText}</span>}
+        </div>
+      </Link>
+      {sideImage && (
+        <div className="left">
+          <Image resolutions="small" src={sideImage} alt="" />
+        </div>
+      )}
+      {backImage && (
+        <div className="right">
+          <Image resolutions="small" src={backImage} alt="" />
+        </div>
+      )}
     </div>
-  </Link>
+  </div>
 )
 
-export default PostCard
+export default LinkContainer

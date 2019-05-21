e89ee2276cdae094fcd79e9e90640e29032d3661
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-05-20 19:07:07 -0500

    Applied cube theme to blog post index.

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,32 +1,87 @@
+// import React from 'react'
+// import { Link } from 'gatsby'
+
+// import Image from './Image'
+// import './PostCard.css'
+
+// const PostCard = ({
+//   featuredImage,
+//   title,
+//   excerpt,
+//   slug,
+//   categories = [],
+//   className = '',
+//   ...props
+// }) => (
+//   <Link to={slug} className={`PostCard ${className}`}>
+//     {featuredImage && (
+//       <div className="PostCard--Image relative">
+//         <Image background src={featuredImage} alt={title} />
+//       </div>
+//     )}
+//     <div className="PostCard--Content">
+//       {title && <h3 className="PostCard--Title">{title}</h3>}
+//       <div className="PostCard--Category">
+//         {categories && categories.map(cat => cat.category).join(', ')}
+//       </div>
+//       {excerpt && <div className="PostCard--Excerpt">{excerpt}</div>}
+//     </div>
+//   </Link>
+// )
+
+// export default PostCard
+
 import React from 'react'
 import { Link } from 'gatsby'
+import Image from './CubeImage'
 
-import Image from './Image'
-import './PostCard.css'
+import './Cubes.css'
 
 const PostCard = ({
   featuredImage,
   title,
   excerpt,
   slug,
-  categories = [],
-  className = '',
+  gist,
+  butttonText,
+  frontImage,
+  sideImage,
+  backImage,
   ...props
 }) => (
-  <Link to={slug} className={`PostCard ${className}`}>
-    {featuredImage && (
-      <div className="PostCard--Image relative">
-        <Image background src={featuredImage} alt={title} />
+  <div key={slug} className="cube-container">
+    <div className="photo-cube">
+      <Link to={slug}>
+        <div className="front photo-desc">
+          {title && <h3>{title}</h3>}
+          <Image resolutions="small" src={featuredImage} alt="" />
+        </div>
+      </Link>
+      <Link to={slug}>
+        <div className="back photo-desc">
+          {title && <h3 className="secondary-header">{title}</h3>}
+          {gist && (
+            <p
+              style={{
+                margin: '42px 0',
+                minHeight: '88px',
+                lineHeight: '1.25'
+              }}
+            >
+              {gist}
+            </p>
+          )}
+          {butttonText && <span>{butttonText}</span>}
+        </div>
+      </Link>
+      <div className="left">
+        <Image resolutions="small" src={featuredImage} alt="" />
       </div>
-    )}
-    <div className="PostCard--Content">
-      {title && <h3 className="PostCard--Title">{title}</h3>}
-      <div className="PostCard--Category">
-        {categories && categories.map(cat => cat.category).join(', ')}
+      <div>
+        <Image resolutions="small" src={featuredImage} alt="" />
       </div>
-      {excerpt && <div className="PostCard--Excerpt">{excerpt}</div>}
     </div>
-  </Link>
+  </div>
 )
 
 export default PostCard

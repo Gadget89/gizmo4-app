69fe930c663784b0e376dbc3a936558791b5497d
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-05-23 09:35:50 -0500

    Added to do for component.

--- src/components/BlogSearch.js
+++ src/components/BlogSearch.js
@@ -3,6 +3,9 @@ import { navigate } from 'gatsby'
 import { Location } from '@reach/router'
 import qs from 'qs'
 
+// To-Do:
+// [ ] Return text "No results found" when there is no results to display
+
 export default ({ pageCount }) => {
   return (
     <Location>

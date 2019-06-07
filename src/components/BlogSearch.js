a542661cfc554a7d68960192c288f22d8c88ddc1
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-06-07 15:11:44 -0500

    Fixed search bar for UI.

--- src/components/BlogSearch.js
+++ src/components/BlogSearch.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { navigate } from 'gatsby'
 import { Location } from '@reach/router'
 import qs from 'qs'
+import { Search } from 'react-feather'
+import './PostCategoriesNav.css'
 
 // To-Do:
 // [ ] Return text "No results found" when there is no results to display
@@ -14,22 +16,25 @@ export default ({ pageCount }) => {
         let search = qs.parse(location.search.replace('?', ''))
 
         return (
-          <input
-            type="text"
-            value={search.s || ''}
-            placeholder="Search..."
-            onChange={e => {
-              let search = {}
-              search.s = e.target.value
-              search = qs.stringify(search)
+          <div className="searchBoxContainer">
+            <input
+              type="text"
+              value={search.s || ''}
+              placeholder="Search..."
+              onChange={e => {
+                let search = {}
+                search.s = e.target.value
+                search = qs.stringify(search)
 
-              const url = location.href
-                .replace(location.origin, '')
-                .replace(location.search, '')
+                const url = location.href
+                  .replace(location.origin, '')
+                  .replace(location.search, '')
 
-              navigate(`${url}?${search}`)
-            }}
-          />
+                navigate(`${url}?${search}`)
+              }}
+            />
+            <Search className="searchBoxIcon" />
+          </div>
         )
       }}
     </Location>

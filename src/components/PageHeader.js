4629cbbc44e662d1eb7fe1b0841351d1520bd1b2
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-06-13 15:45:33 -0500

    Styled blog post hero header layout.

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import _format from 'date-fns/format'
 import PropTypes from 'prop-types'
 
-import { Navigation2 } from 'react-feather'
+import { Navigation2, Clock, Edit } from 'react-feather'
 
 import Image from './Image'
 import Content from './Content'
@@ -37,21 +37,24 @@ const PageHeader = ({
         {date && (
           <div>
             {/* Add icons next to fields in hero image section. */}
-            <p>4 MIN READ</p>
-            <p
-              style={{
-                margin: 0,
-                textTransform: 'uppercase'
-              }}
-            >
-              published
-            </p>
-            <time date={date}>{_format(date, 'MM.DD.YY')}</time>
+            <div>
+              <Clock className="PageHeader--Icon" />
+              <span className="PageHeader--Icon--Text">4 MIN READ</span>
+            </div>
+            <div>
+              <Edit className="PageHeader--Icon" />
+              <span className="PageHeader--Icon--Text">published:</span>
+            </div>
+            <div>
+              <span className="PageHeader-Date">
+                <time date={date}>{_format(date, 'MM.DD.YY')}</time>
+              </span>
+            </div>
           </div>
         )}
         <Navigation2
           style={{
-            margin: '5rem 0',
+            margin: '2rem 0',
             width: '90vw',
             textAlign: 'center',
             transform: 'rotate(180deg)'

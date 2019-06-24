619d4dd405d1aa67cdf9b692db2d99f37c4d9dae
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-06-24 10:46:38 -0500

    Updated cube focus to handle tab key functionality.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './Logo.css'
 
 export default () => (
-  <div
-    className="Logo"
-    style={{
-      backgroundImage: `url(/images/logo.png)`
-    }}
-  />
+  // <div
+  //   className="Logo"
+  //   style={{
+  //     backgroundImage: `url(/images/logo.png)`
+  //   }}
+  // />
+  <h1>curio gadget</h1>
 )

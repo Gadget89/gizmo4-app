0d13bde8a9dc4215d59c43ff156a9bf7b370f180
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-06-27 18:14:52 -0500

    Minor fix to current tab styling.

--- src/components/PostCategoriesNav.js
+++ src/components/PostCategoriesNav.js
@@ -15,7 +15,7 @@ const PostCategoriesNav = ({
     <Link
       className="PostCategoriesTabs"
       exact="true"
-      to={`/blog/`}
+      to={`/blog`}
       activeStyle={activeStyles}
     >
       All
@@ -37,44 +37,3 @@ const PostCategoriesNav = ({
 )
 
 export default PostCategoriesNav
-
-// import React from 'react'
-// import { navigate } from 'gatsby'
-// import { Location } from '@reach/router'
-// import qs from 'qs'
-// import { Search } from 'react-feather'
-// import './PostCategoriesNav.css'
-
-// To-Do:
-// [ ] Return text "No results found" when there is no results to display
-// [ ] Add search exit button and functionality.
-
-// export default ({ pageContent }) => {
-//   return (
-//     <Location>
-//       {({ location }) => {
-//         let currentUrl = location
-
-//         return (
-//           <div className="PostCategoriesNav">
-//     <Link className="PostCategoriesTabs" exact="true" to={`/blog/`}>
-//       All
-//     </Link>
-//     {categories.map((category, index) => (
-//       <Link
-//         exact="true"
-//         className="PostCategoriesTabs"
-//         key={category.title + index}
-//         to={category.slug}
-//       >
-//         {category.title}
-//       </Link>
-//     ))}
-
-//     {enableSearch && <BlogSearch />}
-//   </div>
-//         )
-//       }}
-//     </Location>
-//   )
-// }

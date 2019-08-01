059128d74dbdae03fd75997c260c8efa6352327f
Nicolas Roybal <nicolas.roybal@hotmail.com>
2019-08-01 15:52:38 -0500

    Refactored code and styling for logo component.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,21 +1,4 @@
 import React from 'react'
 import './Logo.css'
 
-export default () => (
-  <div
-    className="Logo"
-    style={{
-      backgroundImage: `url(/images/logo.png)`
-    }}
-  />
-  // <h1
-  //   style={{
-  //     fontSize: '4rem',
-  //     fontWeight: '500',
-  //     textTransform: 'uppercase',
-  //     margin: '0'
-  //   }}
-  // >
-  //   nicolas roybal
-  // </h1>
-)
+export default () => <div className="Logo" />
